1601551107c367d51272d2c94299bd91d5128591
aavnip <aavnip@chicagobooth.edu>
2022-02-21 20:58:28 +0000

    Add user and update username completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,31 @@ class UsersController < ApplicationController
     render({:template=> "user_templates/show.html.erb"}) 
   end
 
+  def create
+    # Parameters: {"query_username"=>"aavni", "path_username"=>":path_username"}
+
+    input_username = params.fetch("query_username")
+    a_new_user = User.new
+    a_new_user.username = input_username
+    a_new_user.save 
+    
+    # render({:template=> "user_templates/create.html.erb"})
+    next_url = "/users/" + a_new_user.username
+    redirect_to(next_url)
+  end
+
+  def update
+    # Parameters: {"query_username"=>"dhruv 2", "modify_username"=>"dhruv"}
+    original_username = params.fetch("modify_username")
+    updated_username = params.fetch("query_username")
+    matching_users = User.where({:username => original_username})
+    the_user = matching_users.at(0)
+    the_user.username = updated_username
+    the_user.save
+
+    next_url = "/users/" + the_user.username
+    redirect_to(next_url)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get("/", {:controller=> "users", :action=> "index"})
   get("/users", {:controller=> "users", :action=> "index"})
   get("/users/:path_username", {:controller=> "users", :action=> "show"})
+  get("/insert_user", {:controller=> "users", :action=> "create"})
+  get("/update_user/:modify_username", {:controller=> "users", :action=> "update"})
 
   get("/photos", {:controller=> "photos", :action=> "index"})
   get("/photos/:path_id", {:controller=> "photos", :action=> "show"})
